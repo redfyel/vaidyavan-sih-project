8ba163e6998bf3c6b5046ce580a25153caf0c890
APPANAHARINI1234 <harini.ap04@gmail.com>
2024-08-30 16:04:11 +0530

    modified dropdown for respinsiveness

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineHome } from 'react-icons/ai';
 import { PiPlantDuotone } from "react-icons/pi";
@@ -6,63 +6,62 @@ import { CiLogin } from 'react-icons/ci';
 import { FaBars } from 'react-icons/fa'; 
 import { GiPlantRoots } from "react-icons/gi";
 import { HiMiniPencilSquare } from "react-icons/hi2";
-
+import './Header.css'; // Ensure to import the CSS file
 
 function Header() {
   const [userLoginStatus, setUserLoginStatus] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
 
   return (
-    <div className="d-flex justify-content-between header" style={{ backgroundColor: '#cfe2d9' }}>  
-       <div className="d-flex justify-content-start logo">
+    <div className="header">
+      <div className="logo">
         <Link to="/">
-          <img src="src\assets\images\vaidyavan-logo.png" alt="Logo Unavailable currently" className='p-3' width={150} height={150} />
+          <img src="src/assets/images/vaidyavan-logo.png" alt="Logo Unavailable currently" className='logo-img' />
         </Link>
-        <h1 className="m-3" style={{ color: '#cfe2d9' }}>VaidyaVan</h1>
+        <h1 className="logo-title">VaidyaVan</h1>
       </div>
-        <nav className="navbar">
-            <button className="navbar-toggler" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                <FaBars />
-            </button>
-            <ul className="nav fs-5 p-3" style={{ display: isMenuOpen ? 'block' : 'none' }}>  
-                <li className="nav-item">
-                    <Link to="/" className="nav-link" style={{ color: '#434343' }}> 
-                        <AiOutlineHome /> Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="learn" className="nav-link" style={{ color: '#434343' }}>  
-                    <PiPlantDuotone /> Learn
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="guided-tours" className="nav-link" style={{ color: '#434343' }}>  
-                    <GiPlantRoots /> Guided Tours
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="register" className="nav-link" style={{ color: '#434343' }}>  
-                    <HiMiniPencilSquare /> Register
-                    </Link>
-                </li>
-                {userLoginStatus === false ? (
-                    <li className="nav-item">
-                        <Link to="login" className="nav-link" style={{ color: '#434343' }}>  
-                            <CiLogin /> Login
-                        </Link>
-                    </li>
-                ) : (
-                    <li className="nav-item">
-                        <Link to="login" className="nav-link" style={{ color: '#434343' }}>  
-                            <CiLogin /> Logout
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </nav>
+      <nav className="navbar">
+        <button className="navbar-toggler" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <FaBars />
+        </button>
+        <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <AiOutlineHome /> Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="learn" className="nav-link">
+              <PiPlantDuotone /> Learn
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="guided-tours" className="nav-link">
+              <GiPlantRoots /> Guided Tours
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="register" className="nav-link">
+              <HiMiniPencilSquare /> Register
+            </Link>
+          </li>
+          {userLoginStatus === false ? (
+            <li className="nav-item">
+              <Link to="login" className="nav-link">
+                <CiLogin /> Login
+              </Link>
+            </li>
+          ) : (
+            <li className="nav-item">
+              <Link to="login" className="nav-link">
+                <CiLogin /> Logout
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
     </div>
-);
+  );
 }
 
 export default Header;
